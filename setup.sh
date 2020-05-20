fa3397db64997edf584e35ae2d29f0d3dd793467
zaimy <zaitsu@pepabo.com>
2020-05-20 14:36:40 +0900

    Add setup steps.

--- setup.sh
+++ setup.sh
@@ -14,3 +14,21 @@ read
 
 echo "====> Install applications with Homebrew..."
 brew bundle
+
+echo "====> Install Poetry..."
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
+
+echo "====> Install Logitech Options..."  # TODO: Use Homebrew after cask updating. https://github.com/Homebrew/homebrew-cask-drivers/pull/1581
+open https://www.logitech.com/en-us/product/options
+echo "Press any keys when the installation is complete."
+read
+
+echo "====> Install Snap camera..."
+open https://snapcamera.snapchat.com/download/
+echo "Press any keys when the installation is complete."
+read
+
+echo "====> Setup MacTex..."
+exec $SHELL -l
+sudo tlmgr update --self --all # Update all packages
+sudo tlmgr paper a4
